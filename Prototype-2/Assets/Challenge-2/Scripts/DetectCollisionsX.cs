60d888b713d4b85cc7f2a5e8d964fd899c6b2183
Val Nunez <val@friendlyhello.com>
2021-06-20 20:56:56 -0700

    ...

---
+++ Prototype-2/Assets/Challenge-2/Scripts/DetectCollisionsX.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectCollisionsX : MonoBehaviour
+{
+
+    // Custom override for OnTriggerEnter
+    private void OnTriggerEnter(Collider other)
+    {
+        //Destroy this game object
+        Destroy(gameObject);
+
+        // Destroy "other" game object (animal)
+        Destroy(other.gameObject);
+    }
+}
